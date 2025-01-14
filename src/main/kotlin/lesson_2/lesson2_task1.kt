a8b430ee2aa21ebc058c94dcf2bb11741ac397bb
iskashin <ivankashin110491@gmail.com>
2025-01-14 23:49:22 +0300

    KS-1-5 Calculation hours & minutes

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_2
+
+fun main(){
+    val ratingPit=3f
+    val ratingTom=4f
+    val ratingBill=3f
+    val ratingAlex=5f
+
+    val averageScore = (ratingPit+ratingTom+ratingBill+ratingAlex)/4
+
+    println(averageScore)
+}
