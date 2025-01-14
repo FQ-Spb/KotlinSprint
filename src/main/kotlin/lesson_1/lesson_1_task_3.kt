9df20de8d15b4bfa64e8e5855850eac2e5da785e
iskashin <ivankashin110491@gmail.com>
2025-01-14 17:03:02 +0300

    KS-1-3 The Chronicles of Gagarin

--- src/main/kotlin/lesson_1/lesson_1_task_3.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -1,6 +1,15 @@
 package org.example.lesson_1
 
 fun main(){
-    println("Тестовое решение к уроку 1 задаче 3")
-    println("Некие исправления")
+   val year = 1961
+    var hour = 9
+    var minute = 7
+    println(year)
+    println(hour)
+    println(minute)
+    hour+=1
+    minute+=48
+    print(hour)
+    print(':')
+    print(minute)
 }
