f7992917920bd2573eedfd97f02be4899dc2024f
iskashin <ivankashin110491@gmail.com>
2025-01-14 16:08:34 +0300

    KS-1-1 initialization of variables

--- src/main/kotlin/lesson_1/lesson_1_task_1.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -2,7 +2,10 @@ package org.example.lesson_1
 
 fun main(){
     val numberOfOrders = 75
+    // val  написал, потому что в случае с var
+    // среда ругается, что переменная не изменяется в коде
     val gratitude = "We LOVE you for buying from our store, but we HATE you for buying so little!"
     println(numberOfOrders)
     println(gratitude)
+    //println добавил опять таки потому, что при попытке отправить коммит среда забухтела что переменные нигде не используются.
 }
