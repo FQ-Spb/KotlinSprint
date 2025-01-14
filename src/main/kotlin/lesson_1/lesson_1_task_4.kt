13232e45d2105774e625e465e11175dab959c637
iskashin <ivankashin110491@gmail.com>
2025-01-14 18:13:13 +0300

    KS-1-4 obvious initialization of variables

---
+++ src/main/kotlin/lesson_1/lesson_1_task_4.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1
+
+fun main(){
+    val length: Long = 40868600000  //тут
+    val age: Byte = 27
+    val partOfDay = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4  //тут
+    val apogee: Int = 327000  // и тут среда при попытке отправить коммит стала ругаться,
+    // что явное объявление в этих местах не требуется.
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
