220ce8fcb11ada3a925b2d1ec8f7930f0343278e
iskashin <ivankashin110491@gmail.com>
2025-01-16 15:50:55 +0300

    KS-3-4 Chess

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main(){
+
+    var from = "E2"
+    var to = "E4"
+    var strokeNum = 1
+    println("[$from-$to;$strokeNum]")
+    from = "D2"
+    to = "D3"
+    strokeNum++
+    println("[$from-$to;$strokeNum]")
+}
