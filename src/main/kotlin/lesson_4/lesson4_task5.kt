30ebb7d76805da00377cc8c7036c8b6dd32bcedb
iskashin <ivankashin110491@gmail.com>
2025-01-17 20:12:33 +0300

    KS-4-5 Solution with logical operators only

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -11,27 +11,12 @@ fun main() {
     val amountOfFood = readlnOrNull()!!.toInt()
     print("Благоприятна ли погода(true или false): ")
     val weather = readlnOrNull().toBoolean()
+    val shipCanSail = (noDamage && (crewSize in 55..70) && (amountOfFood > 50) && weather || !weather) ||
+            (!noDamage && (crewSize == 70) && (amountOfFood >= 50) && weather)
 
-    if (noDamage == false) {
-        println("Повреждения значительны?")
-        val significantDamage = readlnOrNull().toBoolean()
-        if (significantDamage == true) {
-            return println("Корабль плыть не может!")
-        } else {
-            if ((crewSize == 70) && (amountOfFood >= 50) && (weather == true)) {
-                println("Корабль может отплывать!")
-            } else {
-                println("Корабль плыть не может!")
-            }
-        }
+    if (shipCanSail) {
+        println("Корабль может плыть!")
     } else {
-        if ((noDamage == true) && (crewSize >= 55) && (crewSize <= 70) && (amountOfFood > 50) && (weather == true) ||
-            (weather == false)
-        ) {
-            println("Корабль может отплывать!")
-        } else {
-            println("Корабль плыть не может!")
-        }
-
+        println("Корабль плыть не может!")
     }
 }
