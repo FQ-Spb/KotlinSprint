a9d68cd5baf0cbb77a0ff4ed3efbcb28ea03d71b
iskashin <ivankashin110491@gmail.com>
2025-01-17 21:01:47 +0300

    KS-3-2 Autoformatting & branch rename

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -1,10 +1,11 @@
 package org.example.lesson_3
 
-fun main(){
+fun main() {
 
     var from = "E2"
     var to = "E4"
     var strokeNum = 1
+
     println("[$from-$to;$strokeNum]")
     from = "D2"
     to = "D3"
