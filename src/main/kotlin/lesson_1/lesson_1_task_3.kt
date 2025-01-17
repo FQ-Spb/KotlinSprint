9576e1db9fe5cf3fee39c242ef97ef1c6963a8db
iskashin <ivankashin110491@gmail.com>
2025-01-17 15:25:53 +0300

    KS-1-3 Start and landin of Gagarin

--- src/main/kotlin/lesson_1/lesson_1_task_3.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -1,14 +1,15 @@
 package org.example.lesson_1
 
-fun main(){
-   val year = 1961
+fun main() {
+    val year = 1961
     var hour = 9
     var minute = 7
+
     println(year)
-    println(hour)
-    println(minute)
-    hour+=1
-    minute+=48
+    println(String.format("%02d",hour))
+    println(String.format("%02d",minute))
+    hour += 1
+    minute += 48
     print(hour)
     print(':')
     print(minute)
