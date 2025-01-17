ba096802e6c0f27d6fbeda2e77ad444ccc43a4ae
iskashin <ivankashin110491@gmail.com>
2025-01-17 20:52:34 +0300

    KS-3-2 Autoformatting done & branch rename

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -1,10 +1,12 @@
 package org.example.lesson_3
 
-fun main(){
+fun main() {
+
     var personName = "Андреева Татьяна Сергеевна"
     var personAge = 20
+
     println("$personName, $personAge")
-    personAge+=2
+    personAge += 2
     personName = "Сидорова Татьяна Сергеевна"
     println("$personName, $personAge")
 }
