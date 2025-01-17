d929d96de26392a848c80466ec9d3f40c71e9f12
iskashin <ivankashin110491@gmail.com>
2025-01-17 16:09:41 +0300

    KS-2-3 arrival time calculating

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_2
 
-fun main(){
-   val depHour = 9
+fun main() {
+    val depHour = 9
     val depMin = 56
     val travelTime = 457
-    val depInMin = depHour*60+depMin
-    val arrTimeInMin = depInMin+travelTime
-val arrivalHour = arrTimeInMin/60
-    val arrivalMin = arrTimeInMin%60
+    val depInMin = depHour * 60 + depMin
+    val arrTimeInMin = depInMin + travelTime
+    val arrivalHour = arrTimeInMin / 60
+    val arrivalMin = arrTimeInMin % 60
 
-    println(String.format("%02d",arrivalHour)+':'+String.format("%02d",arrivalMin))
-//Если забуду, напомните, пожалуйста, задать вопрос почему у меня не вышло решить эту задачу альтернативным способом.
+    println("${String.format("%02d", arrivalHour)}:${String.format("%02d", arrivalMin)}")
+
 }
