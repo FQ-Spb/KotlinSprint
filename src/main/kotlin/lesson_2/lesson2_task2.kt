6d9d21ca4bb7922c14658487972949031f0cc58f
iskashin <ivankashin110491@gmail.com>
2025-01-17 16:02:21 +0300

    KS-2-2 average salary calculation

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_2
 
-fun main(){
+fun main() {
     val perStaff = 50
     val interns = 30
     val psSalary = 50000
     val intSalary = 20000
-    val totalPsSalary = perStaff*psSalary
-    val payrollFund = perStaff*psSalary+interns*intSalary
-    val averageSal = payrollFund/(perStaff+interns)
-    println(totalPsSalary)
-    println(payrollFund)
-    println(averageSal)
+    val totalPsSalary = perStaff * psSalary
+    val payrollFund = perStaff * psSalary + interns * intSalary
+    val averageSal = payrollFund / (perStaff + interns)
+
+    println("$totalPsSalary \n $payrollFund \n $averageSal")
+
 }
