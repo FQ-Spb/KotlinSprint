70ff7fbcf2ef36826e0859a00563318c57601c9f
iskashin <ivankashin110491@gmail.com>
2025-01-17 21:20:05 +0300

    KS-3-5 Splitting a line

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -1,18 +1,19 @@
 package org.example.lesson_3
 
-fun main(){
+fun main() {
 
     val inputLine = "D2-D4;0"
-
     val list = inputLine.split(";", "-").map { it.trim() }
-
     val from = list.get(0)
     val to = list.get(1)
     val moveNum = list.get(2)
-println("""
+
+    println(
+        """
     $from
     $to
     $moveNum
-""".trimIndent())
+""".trimIndent()
+    )
 
 }
