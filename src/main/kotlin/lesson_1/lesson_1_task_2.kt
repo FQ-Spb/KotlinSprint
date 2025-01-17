f842c17d1f3e9beeba4329157f057359e579dbb4
iskashin <ivankashin110491@gmail.com>
2025-01-17 15:14:21 +0300

    KS-1-2 output gratitude

--- src/main/kotlin/lesson_1/lesson_1_task_2.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -1,13 +1,12 @@
 package org.example.lesson_1
 
-fun main(){
+fun main() {
     val numberOfOrders = 75
-
     val gratitude = "We LOVE you for buying from our store, but we HATE you for buying so little!"
+    var numberOfSmurfs = 2000
+
     println(numberOfOrders)
     println(gratitude)
-    var numberOfSmurfs = 2000
-//    println(numberOfSmurfs)
     numberOfSmurfs--
     println(numberOfSmurfs)
 }
