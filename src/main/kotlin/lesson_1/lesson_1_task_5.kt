56a7af8cbcd685c3c33f18eeacadbb7403209588
iskashin <ivankashin110491@gmail.com>
2025-01-19 00:04:18 +0300

    KS-4-4 Addition constants & magic numbers delete

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -1,12 +1,11 @@
 package org.example.lesson_1
 
 
-
 fun main() {
     val totalTime = 6480
-    val hour = totalTime / 3600
-    val minutes = totalTime % 3600 / 60
-    val seconds = totalTime - hour * 3600 - minutes * 60
+    val hour = totalTime / (SECONDS_IN_HOUR)
+    val minutes = totalTime % SECONDS_IN_HOUR / SECONDS_IN_MINUTE
+    val seconds = totalTime - hour * SECONDS_IN_HOUR - minutes * SECONDS_IN_MINUTE
 
     println(
         String.format("%02d", hour) + ':' + String.format("%02d", minutes) +
@@ -15,3 +14,5 @@ fun main() {
 
 }
 
+const val SECONDS_IN_MINUTE = 60
+const val SECONDS_IN_HOUR = 3600
