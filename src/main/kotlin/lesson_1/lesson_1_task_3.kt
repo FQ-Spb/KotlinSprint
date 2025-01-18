36c57fb695f80de0c5e0bb4b988a63f27cadd1fb
iskashin <ivankashin110491@gmail.com>
2025-01-19 00:21:35 +0300

    KS-1-3 initializatiom variables in type String

--- src/main/kotlin/lesson_1/lesson_1_task_3.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -1,16 +1,14 @@
 package org.example.lesson_1
 
 fun main() {
-    val year = 1961
-    var hour = 9
-    var minute = 7
+    val year = "1961"
+    var hour = "09".toInt()
+    var minute = "07".toInt()
 
     println(year)
-    println(String.format("%02d",hour))
-    println(String.format("%02d",minute))
+    println(String.format("%02d", hour))
+    println(String.format("%02d", minute))
     hour += 1
     minute += 48
-    print(hour)
-    print(':')
-    print(minute)
+    print("$hour:$minute")
 }
