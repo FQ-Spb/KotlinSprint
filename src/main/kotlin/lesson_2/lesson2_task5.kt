568c0d9817f70a6a1517e906917355573e53beb8
iskashin <ivankashin110491@gmail.com>
2025-01-19 23:35:46 +0300

    KS-2-5 Future deposit amount calculation

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -4,9 +4,10 @@ import kotlin.math.pow
 
 fun main() {
     val depositAmount = 70000
-    val interestRate = 16.7
     val depositTerm = 20
-    val futureDepAmount = depositAmount * ((1 + interestRate / 100).pow(depositTerm))
+    val futureDepAmount = depositAmount * ((1 + INTEREST_RATE / 100).pow(depositTerm))
 
     println("%.3f".format(futureDepAmount))
 }
+
+const val INTEREST_RATE = 16.7
