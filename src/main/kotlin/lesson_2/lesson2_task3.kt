7e27312a17a9de2eef0321e8e709cfdec1e8a8e9
iskashin <ivankashin110491@gmail.com>
2025-01-19 23:00:17 +0300

    KS-2-3 Replacing magic numbers

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -4,11 +4,13 @@ fun main() {
     val depHour = 9
     val depMin = 56
     val travelTime = 457
-    val depInMin = depHour * 60 + depMin
+    val depInMin = depHour * MINUTES_IN_HOUR + depMin
     val arrTimeInMin = depInMin + travelTime
-    val arrivalHour = arrTimeInMin / 60
-    val arrivalMin = arrTimeInMin % 60
+    val arrivalHour = arrTimeInMin / MINUTES_IN_HOUR
+    val arrivalMin = arrTimeInMin % MINUTES_IN_HOUR
 
     println("${String.format("%02d", arrivalHour)}:${String.format("%02d", arrivalMin)}")
 
 }
+
+val MINUTES_IN_HOUR = 60
