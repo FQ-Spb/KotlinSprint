c618a56d174930b198a92d493a6041f52767aef1
iskashin <ivankashin110491@gmail.com>
2025-01-19 23:19:27 +0300

    KS-2-4 Replacing magic numbers

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,9 +3,9 @@ package org.example.lesson_2
 fun main() {
     val crystalOre = 7
     val ironOre = 11
-    val crystalOreBaff = (crystalOre * 1.2 - crystalOre).toInt()
-    val ironOreBaff = (ironOre * 1.2 - ironOre).toInt()
 
-    println("Поздравляю,тебе накинули $crystalOreBaff кристалической руды!")
-    println("Поздравляю,тебе накинули $ironOreBaff железной руды!")
+    println("Поздравляю,тебе накинули ${(crystalOre * (BAFF_IN_PERCENT / 100)).toInt()} кристалической руды!")
+    println("Поздравляю,тебе накинули ${(ironOre * (BAFF_IN_PERCENT / 100)).toInt()} железной руды!")
 }
+
+const val BAFF_IN_PERCENT = 20f
