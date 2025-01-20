aee967a928c1663017ce5bb354eff8e31e3066c6
iskashin <ivankashin110491@gmail.com>
2025-01-21 00:21:38 +0300

    KS-2-4 const added

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -4,8 +4,9 @@ fun main() {
     val crystalOre = 7
     val ironOre = 11
 
-    println("Поздравляю,тебе накинули ${(crystalOre * (BAFF_IN_PERCENT / 100)).toInt()} кристалической руды!")
-    println("Поздравляю,тебе накинули ${(ironOre * (BAFF_IN_PERCENT / 100)).toInt()} железной руды!")
+    println("Поздравляю,тебе накинули ${(crystalOre * (BAFF_IN_PERCENT / CONSTANT_DIVISOR)).toInt()} кристалической руды!")
+    println("Поздравляю,тебе накинули ${(ironOre * (BAFF_IN_PERCENT / CONSTANT_DIVISOR)).toInt()} железной руды!")
 }
 
 const val BAFF_IN_PERCENT = 20f
+const val CONSTANT_DIVISOR = 100
