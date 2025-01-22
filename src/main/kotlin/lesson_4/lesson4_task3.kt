8dfd23bad46c4abb501268d9044082037fe71ea0
iskashin <ivankashin110491@gmail.com>
2025-01-22 20:13:09 +0300

    KS-4-3 rename variables and make them const

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,12 +1,7 @@
 package org.example.lesson_4
 
 fun main() {
-    //критерии для роста бобовых
-    val sunny = true
-    val openRoof = true
-    val airHumidity = 20
-    val season = "Winter"
-    // Условия на ферме сегодня
+
     val sunnyToday = true
     val openRoofToday = true
     val airHumidityToday = 20
@@ -14,8 +9,13 @@ fun main() {
 
     println(
         "Благоприятные ли условия сейчас для роста бобовых? ${
-            (sunnyToday == sunny) && (openRoofToday == openRoof) &&
-                    (airHumidityToday == airHumidity) && (seasonToday != season)
+            (sunnyToday == IS_SUNNY) && (openRoofToday == IS_AWNING_OPEN) &&
+                    (airHumidityToday == AIR_HUMIDITY) && (seasonToday != SEASON)
         }"
     )
 }
+
+const val IS_SUNNY = true
+const val IS_AWNING_OPEN = true
+const val AIR_HUMIDITY = 20
+const val SEASON = "Winter"
