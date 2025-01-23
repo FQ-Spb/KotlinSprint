a647a18eff976d28b138723fcff52515482e7ce4
iskashin <ivankashin110491@gmail.com>
2025-01-23 21:41:56 +0300

    KS-5-5 variable which contains number range was added

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -3,11 +3,13 @@ package org.example.lesson_5
 
 fun main() {
 
-    val hiddenNumbers = listOf((0..42).random(), (0..42).random(), (0..42).random())
+    val possibleValues = (0..42)
+    val hiddenNumbers = listOf(possibleValues.random(), possibleValues.random(), possibleValues.random())
 
     println("Введите три числа от 0 до 42:")
     val enteredNumbers = MutableList(3) { readln().toInt() }
     val intersection = hiddenNumbers.intersect(enteredNumbers)
+
     val result = when (intersection.count()) {
         3 -> "Вы угадали все три числа и выиграли джек-пот!"
         2 -> "Вы угадали два числа! Вам положен крупный приз!"
