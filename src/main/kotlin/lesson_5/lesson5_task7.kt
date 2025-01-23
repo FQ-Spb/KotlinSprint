bd9fdf8b5811ae79222fb1915f44d95f8780918e
iskashin <ivankashin110491@gmail.com>
2025-01-23 22:35:36 +0300

    KS-5-7 parameters of last println was reduced

--- src/main/kotlin/lesson_5/lesson5_task7.kt
+++ src/main/kotlin/lesson_5/lesson5_task7.kt
@@ -3,18 +3,24 @@ package org.example.lesson_5
 fun main() {
 
     print("Введите длину пути в километрах: ")
+
     val pathLength = readln().toFloat()
     print("Введите расход топлива вашего автомобиля на 100 км:")
+
     val fuelConsumption = readln().toFloat()
     print("Введите стоимость литра топлива:")
+
     val fuelCost = readln().toFloat()
     val fuelForTrip = pathLength * fuelConsumption / CONSTANT_DIVISOR
     val tripCost = fuelForTrip * fuelCost
 
-    println("Общий расход топлива на $pathLength км " +
-            "${String.format("%.2f", fuelForTrip)}л, стоимостью ${String.format("%.2f", tripCost)} рублей.")
-
-
+    println(
+        String.format(
+            "Общий расход топлива на $pathLength км %.2f л, стоимостью %.2f рублей.",
+            fuelForTrip,
+            tripCost
+        )
+    )
 }
 
 const val CONSTANT_DIVISOR = 100
