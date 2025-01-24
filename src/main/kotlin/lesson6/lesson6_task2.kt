959b93624b706241ef216c8e3659046aab95ff8e
iskashin <ivankashin110491@gmail.com>
2025-01-24 18:59:16 +0300

    KS-6-2 removed the cycle

--- src/main/kotlin/lesson6/lesson6_task2.kt
+++ src/main/kotlin/lesson6/lesson6_task2.kt
@@ -2,16 +2,11 @@ package org.example.lesson6
 
 fun main() {
 
-    var secondsCounter = 0
-
     print("Сколько секунд нужно засечь?")
-    var numberOfEnteredSec = readln().toInt()
+    val numberOfEnteredSec = readln().toLong()
 
-    do {
-        println("${numberOfEnteredSec--}")
-        secondsCounter++
-        Thread.sleep(1000)
-    } while (numberOfEnteredSec > 0)
-    println("Прошло $secondsCounter секунд.")
-    while (numberOfEnteredSec == 0) Thread.sleep(1000)
+    Thread.sleep(numberOfEnteredSec * 1000)
+    println("Прошло $numberOfEnteredSec секунд.")
+
+    while (numberOfEnteredSec >= 0) Thread.sleep(1000)
 }
