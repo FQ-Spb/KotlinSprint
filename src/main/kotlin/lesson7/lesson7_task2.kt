8243d7f8c0b8c4de6a5adb9b87477e5b058f7b37
iskashin <ivankashin110491@gmail.com>
2025-01-24 21:09:22 +0300

    KS-7-2 Declaring a variables outside a cycle

--- src/main/kotlin/lesson7/lesson7_task2.kt
+++ src/main/kotlin/lesson7/lesson7_task2.kt
@@ -1,14 +1,16 @@
 package org.example.lesson7
 
 
-
 fun main() {
 
+    var authCode: Int
+    var userAuthCode: Int
+
     do {
-        val authCode = (1000..9999).random()
+        authCode = (1000..9999).random()
         println("Ваш код авторизиции $authCode")
         print("Введите ваш код авторизации: ")
-        val userAuthCode = readln().toInt()
+        userAuthCode = readln().toInt()
     } while (userAuthCode != authCode)
 
     println("Добро пожаловать!")
