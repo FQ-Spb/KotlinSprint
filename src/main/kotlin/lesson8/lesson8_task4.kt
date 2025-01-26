cbf3aad0e95fcd4ca31af78217aaa5e60cf54d4c
iskashin <ivankashin110491@gmail.com>
2025-01-26 23:25:39 +0300

    KS-8-4 replased .find{} to "in", removed unnecessary loop

--- src/main/kotlin/lesson8/lesson8_task4.kt
+++ src/main/kotlin/lesson8/lesson8_task4.kt
@@ -7,22 +7,19 @@ fun main() {
     for (component in compositionOfOlivie) print("$component ")
     println()
 
-    do {
-        print("Какой ингредиент вы хотели бы заменить?")
-        val userChanges = readln()
+    print("Какой ингредиент вы хотели бы заменить?")
+    val userChanges = readln()
 
-        when (compositionOfOlivie.find { it == userChanges }) {
-            userChanges -> {
-                println("На что хотите заменить?")
-                compositionOfOlivie[compositionOfOlivie.indexOf(compositionOfOlivie.find { it == userChanges })] =
-                    readln()
-                println("Готово! Вы сохранили следующий список: ")
-                for (component in compositionOfOlivie) print("$component ")
-                return
-            }
-
-            else -> println("Такого ингредиента нет в списке.")
+    when (userChanges in compositionOfOlivie) {
+        true -> {
+            println("На что хотите заменить?")
+            compositionOfOlivie[compositionOfOlivie.indexOf(userChanges)] =
+                readln()
+            println("Готово! Вы сохранили следующий список: ")
+            for (component in compositionOfOlivie) print("$component ")
+            return
         }
-    } while (compositionOfOlivie.find { it == userChanges } == null)
 
+        else -> println("Такого ингредиента нет в списке.")
+    }
 }
