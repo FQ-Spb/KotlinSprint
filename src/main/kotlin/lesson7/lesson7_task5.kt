963bf288e2c9df6e2f01e4d7cecf31f3828af8df
iskashin <ivankashin110491@gmail.com>
2025-01-26 22:48:42 +0300

    KS-7-5 separated list to three variables

--- src/main/kotlin/lesson7/lesson7_task5.kt
+++ src/main/kotlin/lesson7/lesson7_task5.kt
@@ -2,20 +2,26 @@ package org.example.lesson7
 
 fun main() {
 
-    val listOfSymbols = listOf((1..9).random(), ('a'..'z').random(), ('A'..'Z').random())
-    var password = charArrayOf()
+    val rangeOfNumbers = ('1'..'9')
+    val rangeOfLowercase = ('a'..'z')
+    val rangeOfUppercase = ('A'..'Z')
+    val password = mutableListOf<Char>()
     var passwordLength: Int
-    val setOfSymbols = (1..9) + ('a'..'z') + ('A'..'Z')
+    val setOfSymbols = rangeOfNumbers + rangeOfLowercase + rangeOfUppercase
+    var result = ""
 
     do {
         print("Введите требуемое количество символов в пароле, но не меньше 6: ")
         passwordLength = readln().toInt()
     } while (passwordLength < 6)
 
-    for (i in listOfSymbols) password.add(i)
+    password.add(rangeOfNumbers.random())
+    password.add(rangeOfUppercase.random())
+    password.add(rangeOfLowercase.random())
 
     while (password.size < passwordLength) password.add(setOfSymbols.random())
+    password.shuffle()
 
-
-
+    for (i in password) result += i
+    println(result)
 }
