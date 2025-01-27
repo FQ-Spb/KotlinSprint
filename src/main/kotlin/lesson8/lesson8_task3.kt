54648140a1c29252542c9f4f54dc3e21bbb9df6e
iskashin <ivankashin110491@gmail.com>
2025-01-27 17:52:14 +0300

    KS-8-3 removed unnecessary file

--- src/main/kotlin/lesson8/lesson8_task3.kt
+++ src/main/kotlin/lesson8/lesson8_task3.kt
@@ -7,6 +7,7 @@ fun main() {
     println("Какой игредиент вы ищете?")
     val lookingForIngredient = readln()
 
+
     val result = when (lookingForIngredient in compositionOfOlivie) {
         true -> "Ингредиент в рецепте есть."
         else -> "Такого ингредиента нет."
