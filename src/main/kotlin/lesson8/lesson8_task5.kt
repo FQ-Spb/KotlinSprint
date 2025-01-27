434a8fbe04262cfea9cc32d36f5d35f9b21725e6
iskashin <ivankashin110491@gmail.com>
2025-01-27 20:06:11 +0300

    KS-8-5 Creating fixed size list of ingredients

--- src/main/kotlin/lesson8/lesson8_task5.kt
+++ src/main/kotlin/lesson8/lesson8_task5.kt
@@ -1,6 +1,15 @@
 package org.example.lesson8
 
-fun main(){
+fun main() {
 
+    println("Какое количество ингредиентов будет в вашем рецепте?")
+    val numberOfIngredients = readln().toInt()
+    val listOfIngredients = Array<String?>(numberOfIngredients) { null }
 
+    for (i in 0 until listOfIngredients.size) {
+        print("Введите ингредиент ${i + 1}: ")
+        listOfIngredients[i] = readln()
+    }
+
+    println(listOfIngredients.joinToString())
 }
