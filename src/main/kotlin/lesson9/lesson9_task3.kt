482169ab50d5ad81db6f9b459233e8f7cf3039ca
iskashin <ivankashin110491@gmail.com>
2025-01-28 19:08:29 +0300

    KS-9-3 calculating and output volue of ingredients with using mutableMapOf & forEach method

--- src/main/kotlin/lesson9/lesson9_task3.kt
+++ src/main/kotlin/lesson9/lesson9_task3.kt
@@ -2,10 +2,15 @@ package org.example.lesson9
 
 fun main() {
 
-   val friedEggsIngredients = mutableMapOf("Яйцо (шт)" to 2,"Молоко (мл)" to 50, "Масло (гр)" to 15)
+    val friedEggsIngredients = mutableMapOf(
+        "Яйцо (шт)" to 2,
+        "Молоко (мл)" to 50,
+        "Масло (гр)" to 15
+    )
 
-   print("Сколько порций вы хотите приготовить?: ")
-   val numberOfPortions = readln().toInt()
-   println("Для приготовдения $numberOfPortions порций вам потребуется: ")
-   friedEggsIngredients.forEach{ (key,value) -> println("$key - ${value*numberOfPortions}")}
+    print("Сколько порций вы хотите приготовить?: ")
+    val numberOfPortions = readln().toInt()
+
+    println("Для приготовдения $numberOfPortions порций вам потребуется: ")
+    friedEggsIngredients.forEach { (key, value) -> println("$key - ${value * numberOfPortions}") }
 }
