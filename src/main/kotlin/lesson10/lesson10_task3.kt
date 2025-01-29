822a871a12f9019f468e70e7d3758f4b4d061326
iskashin <ivankashin110491@gmail.com>
2025-01-29 17:50:38 +0300

    KS-10-3 Creating the function which generate passwords

--- src/main/kotlin/lesson10/lesson10_task3.kt
+++ src/main/kotlin/lesson10/lesson10_task3.kt
@@ -2,11 +2,20 @@ package org.example.lesson10
 
 fun main() {
 
+    print("Введите требуемое количество символов в пароле: ")
 
+    println(passGen(readln().toInt()))
 }
 
-fun passGen(length: Int): Char {
+fun passGen(length: Int): String {
+    val specialSymbols = arrayOf("!", "\"", "#", "$", "%", ".", "&", "'", "(", ")", "*", "+", ",", "-", "/", "\b")
+    val numbers = (0..9)
+    var password = ""
 
-    val password = mutableSetOf<Char>()
-    return 'n'
+    do {
+        password += numbers.random()
+        password += specialSymbols.random()
+    } while (password.length < length)
+
+    return password
 }
