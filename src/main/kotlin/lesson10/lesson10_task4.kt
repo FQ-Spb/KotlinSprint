5ee1a19d9b916e66d45409de367481d56e2bf1b8
iskashin <ivankashin110491@gmail.com>
2025-01-29 18:48:02 +0300

    KS-10-4 [WIP]

--- src/main/kotlin/lesson10/lesson10_task4.kt
+++ src/main/kotlin/lesson10/lesson10_task4.kt
@@ -1,16 +1,34 @@
 package org.example.lesson10
 
+import org.example.lesson9.AGREE
+
 fun main() {
 
+    do {
+        round()
+        println("Хотите сыграть ещё? Введите Да/Нет")
+        val answer = readln()
+    } while (answer.equals(AGREE))
+}
+
+fun round() {
     val throwOfComp = getNumber()
     val throwOfUser = getNumber()
     print("Ход компьютера: ")
-
+    println(throwOfComp)
     print("Ваш ход, нажмите\"ENTER\":")
     readln()
-
+    println(throwOfUser)
+    println(whoWin(throwOfComp, throwOfUser))
 }
 
-const val REFUSAL = "нет"
+fun getNumber() = (1..6).random()
 
-fun getNumber() = println((1..6).random())
+fun whoWin(compNumber: Int, userNumber: Int): String {
+    val result = when {
+        compNumber > userNumber -> "Выиграл Компьютер."
+        compNumber < userNumber -> "Вы выиграли!"
+        else -> "Ничья."
+    }
+    return result
+}
