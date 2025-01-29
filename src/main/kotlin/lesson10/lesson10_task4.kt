52cb906d6d1afa0e14b6048473c97aa91330a32e
iskashin <ivankashin110491@gmail.com>
2025-01-29 18:14:41 +0300

    KS-10-4 [WIP]

--- src/main/kotlin/lesson10/lesson10_task4.kt
+++ src/main/kotlin/lesson10/lesson10_task4.kt
@@ -1,9 +1,16 @@
 package org.example.lesson10
 
-fun main(){
+fun main() {
 
+    val throwOfComp = getNumber()
+    val throwOfUser = getNumber()
+    print("Ход компьютера: ")
+
+    print("Ваш ход, нажмите\"ENTER\":")
+    readln()
 
 }
 
 const val REFUSAL = "нет"
 
+fun getNumber() = println((1..6).random())
