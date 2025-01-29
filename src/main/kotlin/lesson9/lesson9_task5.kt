3c8b87bc89df077cb7c9955fa9788a3b68f5eaa8
iskashin <ivankashin110491@gmail.com>
2025-01-29 17:01:27 +0300

    KS-9-5 replaced .capitalized() method

--- src/main/kotlin/lesson9/lesson9_task5.kt
+++ src/main/kotlin/lesson9/lesson9_task5.kt
@@ -1,13 +1,16 @@
 package org.example.lesson9
 
+import java.util.*
+
 fun main() {
 
     val listOfIngredients = mutableSetOf<String>()
 
     println("Введите пять ингредиентов после каждого нажимая \"ENTER\": ")
     do {
-        listOfIngredients.add(readln())
+        listOfIngredients.add(readln().trim())
     } while (listOfIngredients.size < 5)
 
-    println(listOfIngredients.sorted().joinToString().capitalize())
+    println(listOfIngredients.sorted().joinToString()
+        .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() })
 }
