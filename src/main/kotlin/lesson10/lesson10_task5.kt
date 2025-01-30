a1d308fe49b967523ecb97f379652b5199a2e629
iskashin <ivankashin110491@gmail.com>
2025-01-30 19:07:49 +0300

    KS-10-5 Create an authorization progrem to grend to the shopping basket

---
+++ src/main/kotlin/lesson10/lesson10_task5.kt
@@ -0,0 +1,42 @@
+package org.example.lesson10
+
+
+const val CORRECT_LOGIN = "Ivan"
+const val CORRECT_PASSWORD = "Kashin"
+const val TOKEN_LENGTH = 32
+fun main() {
+
+    print("Ведите свой Логин: ")
+    val inputLogin = readln()
+    print("Введите свой пароль: ")
+    val inputPassword = readln()
+    when (authorize(inputLogin, inputPassword)) {
+        null -> println("Вы ввели неверный логин или пароль.")
+        else -> println(getShopBasket(authorize(inputLogin, inputPassword)).joinToString(",\n"))
+    }
+
+}
+
+fun authorize(login: String, password: String): String? {
+    val stringToken: String
+    val setOfSymbols = ('1'..'9') + ('a'..'z') + ('A'..'Z')
+    val token = Array<Char?>(32) { null }
+    if (login != CORRECT_LOGIN || password != CORRECT_PASSWORD) return null
+    for (element in token.indices) {
+        token[element] = setOfSymbols.random()
+    }
+    token.shuffle()
+    stringToken = token.joinToString("")
+    return stringToken
+}
+
+fun getShopBasket(token: String?): List<String?> {
+    val error = listOf(
+        "Токен: invalidToken\n" +
+                "Ошибка доступа к корзине."
+    )
+    val shoppingBasket = mutableListOf("Ёршик для пупка", "Тапочки для таракана", "Картонный сейф", "Бумажный аквариум")
+
+    return if (token?.length == TOKEN_LENGTH) shoppingBasket
+    else error
+}
