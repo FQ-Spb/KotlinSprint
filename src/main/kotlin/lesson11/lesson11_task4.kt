8ebe77e29b000d3ad7d9bf23f96ecc6cdf59aa9f
iskashin <ivankashin110491@gmail.com>
2025-01-31 21:22:36 +0300

    KS-11-4 [WIP]

--- src/main/kotlin/lesson11/lesson11_task4.kt
+++ src/main/kotlin/lesson11/lesson11_task4.kt
@@ -1,11 +1,57 @@
 package org.example.lesson11
 
 class Caterories(
-    val coverURL:String,
-    val title:String,
-    val descriptionOfCat:String,
-)
+    private var coverURL: String,
+    private var title: String,
+    private var description: String,
+    var recipes: MutableList<Recipe> = mutableListOf(),
+) {
+    fun changeCover(newImage: String) {
+        coverURL = newImage
+    }
+
+    fun changeTitle(newTitleText: String) {
+        title = newTitleText
+    }
+
+    fun changeDescription(newDescText: String) {
+        description = newDescText
+    }
+}
 
 class Recipe(
+    val id: Int,
+    private var title: String,
+    private var coverURL: String,
+    private var description: String,
+    val caloricContent: Int,
+    val ingredients: MutableList<Ingredients> = mutableListOf(),
+    var cookingProcess:String,
+    var isInFavorite: Boolean = false,
+) {
+    fun changeTitle(newTitleText: String) {
+        title = newTitleText
+    }
+    fun changeCover(newImageUrl:String){
+        coverURL= newImageUrl
+    }
+    fun changeDescription(newDescText:String){
+        description=newDescText
+    }
+    fun addInFavorite(){
+        isInFavorite=true
+    }
+    fun removeFromFavorite(){
+        isInFavorite=false
+    }
+    fun changeCookingProcess(newText:String){
+        cookingProcess = newText
+    }
+    fun addIngredient(newIngredient:String){
 
-)
+    }
+}
+
+class Ingredients() {
+
+}
