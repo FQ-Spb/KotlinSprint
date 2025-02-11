68ba6814de55ed0334692b0565f03608e6d357d9
iskashin <ivankashin110491@gmail.com>
2025-02-11 20:12:05 +0300

    KS-17-5 Creating setters and getters. Using .repeat() method.

--- src/main/kotlin/lesson17/lesson17_task5.kt
+++ src/main/kotlin/lesson17/lesson17_task5.kt
@@ -1,12 +1,28 @@
 package org.example.lesson17
 
 class User(
-    private var _login: String,
+    private var _login: String?,
     private val _password: String,
 ) {
-    var login = ""
+    var login: String? = null
         set(value) {
-            _login = value
+            field = value
+            _login = login
             println("Login successfully updated")
         }
+    var password
+        get() = "*".repeat(_password.length)
+        set(value) {
+            println("You can not change password.")
+        }
+}
+
+fun main() {
+    val user1 = User("Ivan", "password123")
+
+    println(user1.password)
+    user1.password = "123"
+    println(user1.password)
+    user1.login = "John"
+    println("Your new login is: \"${user1.login}\"")
 }
