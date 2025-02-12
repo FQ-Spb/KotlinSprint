51e837a5011a87040778813ef0e73675d7352195
iskashin <ivankashin110491@gmail.com>
2025-02-12 17:17:43 +0300

    KS-18-2 Implementing method according to the principle subtype polymorphism

--- src/main/kotlin/lesson18/lesson18_task2.kt
+++ src/main/kotlin/lesson18/lesson18_task2.kt
@@ -8,26 +8,32 @@ open class Dice1(
 
 class TetrahedralDice(
     numberOfEdge: Int = 4,
-    private val cameUpNumber:Int = (1..4).random()
+    private val cameUpNumber: Int = (1..4).random(),
 ) : Dice1(numberOfEdge) {
     override fun throwDice(): String = "Came up number: $cameUpNumber"
 }
 
 class HexagonalDice(
     numberOfEdge: Int = 6,
-    private val cameUpNumber:Int = (1..6).random(),
+    private val cameUpNumber: Int = (1..6).random(),
 ) : Dice1(numberOfEdge) {
-    override fun throwDice():String = "Came up number: $cameUpNumber"
+    override fun throwDice(): String = "Came up number: $cameUpNumber"
 }
 
 class OctahedralDice(
     numberOfEdge: Int = 8,
-    private val cameUpNumber:Int = (1..8).random(),
+    private val cameUpNumber: Int = (1..8).random(),
 ) : Dice1(numberOfEdge) {
-    override fun throwDice():String = "Came up number: $cameUpNumber"
+    override fun throwDice(): String = "Came up number: $cameUpNumber"
 }
 
 fun main() {
-    val dice = Dice1(4)
+    val dice: Dice1 = TetrahedralDice()
+    val dice2: Dice1 = HexagonalDice()
+    val dice3: Dice1 = OctahedralDice()
+
+    val kitOfDices = arrayOf<Dice1>(dice, dice2, dice3)
+
+    kitOfDices.forEach { println(it.throwDice()) }
 
 }
