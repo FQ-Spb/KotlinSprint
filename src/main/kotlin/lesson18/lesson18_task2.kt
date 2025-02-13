fe8735cb12b37439466e2aaed946b2a4352634c6
iskashin <ivankashin110491@gmail.com>
2025-02-13 18:09:46 +0300

    KS-18-2 Removed unnecessary methods and variables

--- src/main/kotlin/lesson18/lesson18_task2.kt
+++ src/main/kotlin/lesson18/lesson18_task2.kt
@@ -1,39 +1,29 @@
 package org.example.lesson18
 
-open class Dice1(
+abstract class Dice1(
     val numberOfEdge: Int,
 ) {
-    open fun throwDice() = ""
+    fun throwDice(numberOfEdge: Int) = (1..numberOfEdge).random()
 }
 
 class TetrahedralDice(
     numberOfEdge: Int = 4,
-    private val cameUpNumber: Int = (1..4).random(),
-) : Dice1(numberOfEdge) {
-    override fun throwDice(): String = "Came up number: $cameUpNumber"
-}
+) : Dice1(numberOfEdge)
 
 class HexagonalDice(
     numberOfEdge: Int = 6,
-    private val cameUpNumber: Int = (1..6).random(),
-) : Dice1(numberOfEdge) {
-    override fun throwDice(): String = "Came up number: $cameUpNumber"
-}
+) : Dice1(numberOfEdge)
 
 class OctahedralDice(
     numberOfEdge: Int = 8,
-    private val cameUpNumber: Int = (1..8).random(),
-) : Dice1(numberOfEdge) {
-    override fun throwDice(): String = "Came up number: $cameUpNumber"
-}
+) : Dice1(numberOfEdge)
 
 fun main() {
     val dice: Dice1 = TetrahedralDice()
     val dice2: Dice1 = HexagonalDice()
     val dice3: Dice1 = OctahedralDice()
-
     val kitOfDices = arrayOf<Dice1>(dice, dice2, dice3)
 
-    kitOfDices.forEach { println(it.throwDice()) }
+    kitOfDices.forEach { println(it.throwDice(it.numberOfEdge)) }
 
 }
