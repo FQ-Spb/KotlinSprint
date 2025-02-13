cb0a8d8b55d790d715103777c7702ccba4c2a103
iskashin <ivankashin110491@gmail.com>
2025-02-13 21:50:51 +0300

    KS-19-4 Selecting ammunition for tank with use of enum class

--- src/main/kotlin/lesson19/lesson19_task4.kt
+++ src/main/kotlin/lesson19/lesson19_task4.kt
@@ -1,11 +1,29 @@
 package org.example.lesson19
 
-enum class Ammunition(damageAmount:Int){
+enum class Ammunition(val damageAmount: Int) {
     BLUE(5),
     GREEN(10),
     RED(20),
 }
 
-class Tank(){
+class Tank(
+    private var typeOfAmmunition: Ammunition? = null,
+) {
+    fun charge(id: Int) {
+        typeOfAmmunition = Ammunition.entries[id]
+        println("$typeOfAmmunition ammo loaded.")
+    }
 
+    fun shoot() = println("Tank fired a ${typeOfAmmunition?.name} shell. Enemy damage: ${typeOfAmmunition?.damageAmount}")
+}
+
+fun main() {
+    val t34 = Tank()
+
+    t34.charge(0)
+    t34.shoot()
+    t34.charge(1)
+    t34.shoot()
+    t34.charge(2)
+    t34.shoot()
 }
