cd99dd26a5d968341c7ccb9c891034d846981e8c
iskashin <ivankashin110491@gmail.com>
2025-02-15 22:37:24 +0300

    KS-19-4 The type of value accepted by the .charge() method has been changed

--- src/main/kotlin/lesson19/lesson19_task4.kt
+++ src/main/kotlin/lesson19/lesson19_task4.kt
@@ -9,21 +9,22 @@ enum class Ammunition(val damageAmount: Int) {
 class Tank(
     private var typeOfAmmunition: Ammunition? = null,
 ) {
-    fun charge(id: Int) {
-        typeOfAmmunition = Ammunition.entries[id]
-        println("$typeOfAmmunition ammo loaded.")
+    fun charge(type: Ammunition) {
+        typeOfAmmunition = type
+        println("${typeOfAmmunition?.name} ammo loaded.")
     }
 
-    fun shoot() = println("Tank fired a ${typeOfAmmunition?.name} shell. Enemy damage: ${typeOfAmmunition?.damageAmount}")
+    fun shoot() =
+        println("Tank fired a ${typeOfAmmunition?.name} shell. Enemy damage: ${typeOfAmmunition?.damageAmount}")
 }
 
 fun main() {
     val t34 = Tank()
 
-    t34.charge(0)
+    t34.charge(Ammunition.BLUE)
     t34.shoot()
-    t34.charge(1)
+    t34.charge(Ammunition.RED)
     t34.shoot()
-    t34.charge(2)
+    t34.charge(Ammunition.GREEN)
     t34.shoot()
 }
