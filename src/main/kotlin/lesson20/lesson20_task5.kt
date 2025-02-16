2fb48de820d637a7c31d74d0dce41278a18a3c6b
iskashin <ivankashin110491@gmail.com>
2025-02-16 22:55:33 +0300

    KS-20-5 Class Robot with modifier property was created

--- src/main/kotlin/lesson20/lesson20_task5.kt
+++ src/main/kotlin/lesson20/lesson20_task5.kt
@@ -1,26 +1,24 @@
 package org.example.lesson20
 
-interface RobotSkills {
-    fun say()
-    fun setModifier(modifier: (String) -> String)
+class Robot(
+
+    private val listOfPhrases: List<String> =
+        listOf("Let's go working.", "I'm tired", "Pour me a cup of oil", "Leave me alone, the leather bag"),
+    private var phrase: String = listOfPhrases[(0..listOfPhrases.lastIndex).random()],
+    private var modifier: (String) -> String = { it },
+) {
+    fun say() = println(modifier(phrase))
+
+    fun setModifier(modifier: (String) -> String) {
+        this.modifier = modifier
+    }
 }
 
 fun main() {
 
-    val modifier = { it: String -> it.reversed() }
-    val listOfPhrases =
-        listOf("Let's go working.", "I'm tired", "Pour me a cup of oil", "Leave me alone, the leather bag")
-    var phrase = listOfPhrases[(0..listOfPhrases.lastIndex).random()]
-    val robot = object : RobotSkills {
-        override fun say() = println(phrase)
+    val robot1 = Robot()
 
-        override fun setModifier(modifier: (String) -> String) {
-            phrase = modifier(phrase)
-        }
-    }
-
-    robot.say()
-    robot.setModifier(modifier)
-    robot.say()
-
+    robot1.say()
+    robot1.setModifier(modifier = { it: String -> it.reversed() })
+    robot1.say()
 }
