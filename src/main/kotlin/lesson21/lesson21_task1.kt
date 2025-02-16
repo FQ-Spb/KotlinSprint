f8ec7086142da77c53698f3141cbf92dee38bfde
iskashin <ivankashin110491@gmail.com>
2025-02-16 23:15:51 +0300

    KS-21-1 method .toRegex() was replaced to this.count{it in vowels}

--- src/main/kotlin/lesson21/lesson21_task1.kt
+++ src/main/kotlin/lesson21/lesson21_task1.kt
@@ -1,13 +1,13 @@
 package org.example.lesson21
 
-fun main(){
+fun main() {
 
-val testString = "Привет меня зовут Иван."
-   println( testString.vowelCount(testString))
+    val testString = "Привет меня зовут Иван."
+
+    println(testString.vowelCount())
 }
 
-fun String.vowelCount(inputString:String):Int{
-    val vowels = "[аоуыэеёиюяАОУЫЭЕЁИЮЯ]".toRegex()
-    val listOfVowels = vowels.findAll(inputString).joinToString(""){it.value}
-    return listOfVowels.length
+fun String.vowelCount(): Int {
+    val vowels = "аоуыэеёиюяАОУЫЭЕЁИЮЯ"
+    return this.count { it in vowels }
 }
